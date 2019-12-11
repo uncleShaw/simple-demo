14be7394a8fb21afcd913a79cd388edf1f1952d6
uncleShaw <nuncleShaw@163.com>
2019-12-11 18:21:57 +0800

    初次提交

---
+++ src/main/java/com/shaw/readremote/ReadremoteApplication.java
@@ -0,0 +1,13 @@
+package com.shaw.readremote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadremoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadremoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shaw/readremote/ReadremoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaw.readremote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadremoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
